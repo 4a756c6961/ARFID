7276f00995d99f895bf813614e0624c79dc9231f
4a756c6961 <julia.jessing@iubh-fernstudium.de>
2025-01-26 10:04:50 +0100

    final

--- app/src/main/java/com/example/arfid/ParentActivity.kt
+++ app/src/main/java/com/example/arfid/ParentActivity.kt
@@ -1,10 +1,13 @@
 package com.example.arfid
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -13,18 +16,26 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -33,17 +44,6 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.arfid.ui.theme.ARFIDTheme
-import android.content.Intent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.NavigationBarItemDefaults
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 
 
 class ParentActivity : ComponentActivity() {
@@ -90,7 +90,7 @@ fun ParentHomeScreen() {
             .verticalScroll(scrollState)
             .padding(16.dp)
     ) {
-        ElevatedCardContent(
+        ElevatedCardContents(
             imageRes = R.drawable.arfid_verstehen,
             title = stringResource(id = R.string.title_arfid_verstehen),
             subtitle = stringResource(id = R.string.subtitle_arfid_verstehen),
@@ -99,7 +99,7 @@ fun ParentHomeScreen() {
             context.startActivity(intent)})
 
         Spacer(modifier = Modifier.height(16.dp))
-        ElevatedCardContent(
+        ElevatedCardContents(
             imageRes = R.drawable.kind_unterstuetzen,
             title = stringResource(id = R.string.title_kind_unterstuetzen),
             subtitle = stringResource(id = R.string.subtitle_kind_unterstuetzen),
@@ -107,7 +107,7 @@ fun ParentHomeScreen() {
                 context.startActivity(intent)})
 
         Spacer(modifier = Modifier.height(16.dp))
-        ElevatedCardContent(
+        ElevatedCardContents(
             imageRes = R.drawable.erfahrungsberichte,
             title = stringResource(id = R.string.title_erfahrungsberichte),
             subtitle = stringResource(id = R.string.subtitle_erfahrungsberichte),
@@ -116,7 +116,7 @@ fun ParentHomeScreen() {
 }
 
 @Composable
-fun ElevatedCardContent(imageRes: Int, onClick: () -> Unit, title: String, subtitle: String ) {
+fun ElevatedCardContents(imageRes: Int, onClick: () -> Unit, title: String, subtitle: String ) {
     ElevatedCard(
         modifier = Modifier
             .fillMaxWidth()
